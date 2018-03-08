fb9cc05e69edb1ba262f677f3521cd70065b8149
dhowe <daniel@rednoise.org>
2018-03-08 16:05:41 +0800

    added simple server

---
+++ server/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace Dialogic.Server
+{
+    public class WebServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public WebServer(IReadOnlyCollection<string> prefixes, Func<HttpListenerRequest, string> method)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
+            }
+
+            // URI prefixes are required eg: "http://localhost:8080/test/"
+            if (prefixes == null || prefixes.Count == 0)
+            {
+                throw new ArgumentException("URI prefixes are required");
+            }
+
+            if (method == null)
+            {
+                throw new ArgumentException("responder method required");
+            }
+
+            foreach (var s in prefixes)
+            {
+                _listener.Prefixes.Add(s);
+            }
+
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+           : this(prefixes, method)
+        {
+        }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem(c =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                if (ctx == null)
+                                {
+                                    return;
+                                }
+
+                                var rstr = _responderMethod(ctx.Request);
+                                var buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch
+                            {
+                                // ignored
+                            }
+                            finally
+                            {
+                                // always close the stream
+                                if (ctx != null)
+                                {
+                                    ctx.Response.OutputStream.Close();
+                                }
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch (Exception)
+                {
+                    // ignored
+                }
+            });
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+
+    public class Program
+    {
+        public static string SendResponse(HttpListenerRequest request)
+        {
+            return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*******************************************");
+            var ws = new WebServer(SendResponse, "http://localhost:8083/test/");
+            ws.Run();
+            Console.WriteLine("A simple webserver. Press a key to quit.");
+            Console.ReadKey();
+            ws.Stop();
+        }
+    }
+}
