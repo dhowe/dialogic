a58f91d098094c0a4680be4a0ec78169c043b76a
dhowe <daniel@rednoise.org>
2018-04-04 02:39:26 +0800

    update doc version

--- docs/html/search/variables_3.js
+++ docs/html/search/variables_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['find_5fmax_5fstaleness',['FIND_MAX_STALENESS',['../class_dialogic_1_1_defaults.html#ab56fd4a024e341edeae89f4fe382d1d9',1,'Dialogic::Defaults']]],
+  ['find_5fnormalize_5fscores',['FIND_NORMALIZE_SCORES',['../class_dialogic_1_1_defaults.html#a2617e2e2d427a1c553717033cf10b323',1,'Dialogic::Defaults']]],
   ['find_5fstaleness',['FIND_STALENESS',['../class_dialogic_1_1_defaults.html#a0fbad28c55447a0f8c3c5fb18b4bd227',1,'Dialogic::Defaults']]]
 ];
