16c2eeb54fce2d9428731c873bb77ab72de15497
dhowe <daniel@rednoise.org>
2018-04-11 03:11:12 +0800

    Release v0.4.86

--- docs/html/search/variables_1.js
+++ docs/html/search/variables_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['chat_5fenable_5fsmoothing',['CHAT_ENABLE_SMOOTHING',['../class_dialogic_1_1_defaults.html#ac0d6214e5d44c308d05417609cad194e',1,'Dialogic::Defaults']]],
   ['chat_5fstaleness',['CHAT_STALENESS',['../class_dialogic_1_1_defaults.html#a3140b6c1bd7fe352f3651fbe856cb6dd',1,'Dialogic::Defaults']]],
   ['chat_5fstaleness_5fincr',['CHAT_STALENESS_INCR',['../class_dialogic_1_1_defaults.html#af715b12c9d2b7ca800bd8e09984a58d3',1,'Dialogic::Defaults']]]
 ];
