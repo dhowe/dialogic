89a96196d84a3d3f70eff2418130f0ac314288ea
dhowe <daniel@rednoise.org>
2018-05-09 02:16:59 +0800

    doc update

--- make-version.sh
+++ make-version.sh
@@ -12,9 +12,10 @@ fi
 export version=$1
 doxygen docs/Doxyfile 2>&1 | grep -v "Unexpected html tag <img>"
 perl -i.bak -p0e 's%<p><a href="https://travis.*?</object>\n</div>%%igs' docs/html/index.html
-perl -i.bak -p0e 's%: Dialogic</title>% v$ENV{version}</title>%igs' docs/html/index.html
+perl -i.bak -p0e 's%Dialogic :fish:</title>% v$ENV{version}</title>%igs' docs/html/index.html
+perl -i.bak -p0e 's%:fish:%%igs' docs/html/index.html
 rm docs/html/index.html.bak
 
 perl -p0e 's%Dialogic</title>%Dialogic v$ENV{version}</title>%igs' editor/data/template.html > editor/data/index.html
 
-#head weblint/data/index.html
+head -n 70 docs/html/index.html
