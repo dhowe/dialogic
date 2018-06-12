dadbd0bce4ea18adf14d6e1c26471c7c1b1f26bc
dhowe <daniel@rednoise.org>
2018-06-12 21:56:43 +0800

    cleanup

--- editor/restart-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#ssh -t $RED "cd /var/www/html/dialogic-gh && git stash && git pull && cd editor && dotnet run rednoise.org"
-ssh -t $RED "cd /var/www/html/dialogic-gh && git stash && git pull && msbuild"
-ssh -t $RED "systemctl restart dialogic-editor.service"
