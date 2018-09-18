2d2d7f2bdb36126f95d5545bc44138453535b43d
dhowe <daniel@rednoise.org>
2018-09-18 18:49:48 +0800

    update event tests

--- tests/EventTests.cs
+++ tests/EventTests.cs
@@ -10,44 +10,8 @@ namespace Dialogic
     [TestFixture]
     public class EventTests : GenericTests
     {
-
-        // EVENTS: StalenessUpdate, ResumeEvent
-        // ClearEvent, ChatUpdate, UserEvent
-        // ChoiceEvent, SuspendEvent
-
-
         // Tendar: SuspendEvent, ResumeEvent, ChoiceEvent, SaveEvent
-
-
-        [Test]
-        public void ChoiceEventTest()
-        {
-        }
-
-        [Test]
-        public void SuspendEventTest()
-        {
-        }
-
-        [Test]
-        public void ResumeEventTest()
-        {
-            string[] lines = {
-                "CHAT c1 {type=a}",
-                "CHAT c2 {type=b,day=fri}",
-                "CHAT c3 {type=c,day=thurs}",
-            };
-
-            string contents = String.Join("\n", lines);
-            List<Chat> chats = ChatParser.ParseText(contents, NO_VALIDATORS);
-            ChatRuntime rt = new ChatRuntime(chats);
-
-            for (int i = 0; i < 3; i++)
-            {
-                EventArgs icu = new ResumeEvent("{}");
-                rt.Update(null, ref icu);
-            }
-        }
+        // Other: ClearEvent, ChatUpdate, UserEvent
 
         [Test]
         public void StalenessUpdateTest()
@@ -77,13 +41,47 @@ namespace Dialogic
             icu = new StalenessUpdate(10, "{!type=a}");
             rt.Update(null, ref icu);
 
-            new AutoResetEvent(false).WaitOne(20); // async hack for C# 4.0
+            new AutoResetEvent(false).WaitOne(20); // async hack
 
             //chats.ForEach(Console.WriteLine);
             Assert.That(rt.FindChatByLabel("c1").Staleness(), Is.EqualTo(10));
             Assert.That(rt.FindChatByLabel("c2").Staleness(), Is.EqualTo(10));
             Assert.That(rt.FindChatByLabel("c3").Staleness(), Is.EqualTo(5));
             Assert.That(rt.FindChatByLabel("c4").Staleness(), Is.EqualTo(100));
+        }
+
+        public void ChoiceEventTest()
+        {
+            // TODO
+
+        }
+
+        [Test]
+        public void SuspendEventTest()
+        {
+            // TODO
+
+        }
+
+        //[Test]
+        public void ResumeEventTest()
+        {
+            // TODO
+            string[] lines = {
+                "CHAT c1 {type=a}",
+                "CHAT c2 {type=b,day=fri}",
+                "CHAT c3 {type=c,day=thurs}",
+            };
+
+            string contents = String.Join("\n", lines);
+            List<Chat> chats = ChatParser.ParseText(contents, NO_VALIDATORS);
+            ChatRuntime rt = new ChatRuntime(chats);
+
+            for (int i = 0; i < 3; i++)
+            {
+                EventArgs icu = new ResumeEvent("{}");
+                rt.Update(null, ref icu);
+            }
         }
 
         [Test]
