4da78ad5a91ec01de54dc37d7e369d4397fa97d2
dhowe <daniel@rednoise.org>
2018-10-04 20:48:17 +0800

    added IParser

---
+++ parser/SuperParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Dialogic;
+
+namespace parser
+{
+    public class SuperParser
+    {
+        private readonly ChatRuntime runtime;
+
+        internal SuperParser(ChatRuntime runtime)
+        {
+            this.runtime = runtime;
+        }
+    }
+}
